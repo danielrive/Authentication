0d65ed512f24f804c2f8925262bb877162d8ca7b
Daniel German RIvera <danielrivera846@gmail.com>
2018-11-19 14:04:52 -0500

    Firts commit-add npm packages and libraries into code

---
+++ login.js
@@ -0,0 +1,6 @@
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+const AWS = require('aws-sdk');
+const request = require('request')
+const jwkToPem = require('jwk-to-pem');
+
